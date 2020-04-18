08f9607640ef65f15a08c091f96ed8b3625c9d80
luminilion <nico.darge@gmail.com>
2020-04-18 16:17:32 +0200

    Mec faut pas indenter avec le cul, en vrai on devrait faire attention à indenter psk sinon c'est illisible

--- src/pages/utils/UtilsSlides.tsx
+++ src/pages/utils/UtilsSlides.tsx
@@ -22,64 +22,63 @@ interface ContainerProps {
 const UtilsSlides : React.FC<ContainerProps> = ({ name }) => {
   return (
     <IonCard href="/royalties">
+
       <IonCardHeader>
           <IonCardSubtitle>{name}</IonCardSubtitle>
           <IonCardTitle>{name}</IonCardTitle>
       </IonCardHeader>
+
       <IonCardContent>
           Les {name}, c'est vraiment trop super. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum, velit id tincidunt pulvinar, felis odio blandit mi, id commodo met
       </IonCardContent>
+
       <IonSlides  options={{slidesPerView:2.5}}>
-      <IonSlide>
-        <IonCard href="/investir">
-          <IonItem>
-            <img src="eiffel.jpg"></img>
-          </IonItem>
-          <IonCardContent>
-            Investir dans mon super projet de tour eiffel
-          </IonCardContent>
-        </IonCard>
-      </IonSlide>
 
-    <IonSlide>
+        <IonSlide>
+          <IonCard href="/investir">
+            <IonItem>
+              <img src="eiffel.jpg"></img>
+            </IonItem>
+            <IonCardContent>
+              Investir dans mon super projet de tour eiffel
+            </IonCardContent>
+          </IonCard>
+        </IonSlide>
 
-      <IonCard href="/investir">
-        <IonItem>
-          <img src="eiffel.jpg"></img>
-        </IonItem>
+        <IonSlide>
+          <IonCard href="/investir">
+            <IonItem>
+              <img src="eiffel.jpg"></img>
+            </IonItem>
+            <IonCardContent>
+              Investir dans mon super projet de tour eiffel
+            </IonCardContent>
+          </IonCard>
+        </IonSlide>
 
-        <IonCardContent>
-          Investir dans mon super projet de tour eiffel
-        </IonCardContent>
-      </IonCard>
-    </IonSlide>
-    <IonSlide>
+        <IonSlide>
+          <IonCard href="/investir">
+            <IonItem>
+              <img src="eiffel.jpg"></img>
+            </IonItem>
+            <IonCardContent>
+              Investir dans mon super projet de tour eiffel
+            </IonCardContent>
+          </IonCard>
+        </IonSlide>
 
-
-      <IonCard href="/investir">
-        <IonItem>
-          <img src="eiffel.jpg"></img>
-        </IonItem>
-
-        <IonCardContent>
-          Investir dans mon super projet de tour eiffel
-        </IonCardContent>
-      </IonCard>
-    </IonSlide>
-    <IonSlide>
-
-
-      <IonCard href="/investir">
-        <IonItem>
-          <img src="eiffel.jpg"></img>
-        </IonItem>
-
-        <IonCardContent>
-          Investir dans mon super projet de tour eiffel
-        </IonCardContent>
-      </IonCard>
-    </IonSlide>
-  </IonSlides>
+        <IonSlide>
+          <IonCard href="/investir">
+            <IonItem>
+              <img src="eiffel.jpg"></img>
+            </IonItem>
+            <IonCardContent>
+              Investir dans mon super projet de tour eiffel
+            </IonCardContent>
+          </IonCard>
+        </IonSlide>
+        
+      </IonSlides>
     </IonCard>
 
   );

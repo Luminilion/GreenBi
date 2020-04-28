1c8dd437f5514f44b7b2428041aba5fa94c58082
Louis de Puysegur <l.de.puysegur@gmail.com>
2020-04-28 15:27:46 +0200

    contract basis

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,6 +24,7 @@ import MainMenu from './components/utils/MainMenu';
 import LeveeFond from './pages/LeveeFond';
 import PortfolioDetail from './pages/PortfolioDetail';
 
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -70,6 +71,7 @@ const App: React.FC = () => (
           <Route path="/contrat" component={Contract} exact={true} />
           <Route path="/document-administratif" component={AdministrativeDocument} exact={true} />
           <Route path="/levee-fond/:projectType/:projectId" component={LeveeFond} exact={true} />
+          <Route path="/contract" component={Contract} exact={true} />
           <Route path="/" render={() => <Redirect to="/login" />} exact={true} />
         </IonRouterOutlet>
     </IonReactRouter>

f054ff34dbba9b7d31cd0fcb57276de91092cfd4
Louis de Puysegur <l.de.puysegur@gmail.com>
2020-04-28 13:21:34 +0200

    text-align

--- client/src/pages/PaymentSuccess.tsx
+++ client/src/pages/PaymentSuccess.tsx
@@ -37,6 +37,7 @@ const PaymentSuccess: React.FC<PaymentSuccessProps> = ({match}) => {
       <IonContent class="payment-success">
         <IonCard class="card-message">
           <h1>Paiement accepté</h1>
+          <p>Un mail de confirmation vous sera envoyé</p>
           <IonIcon color="success" icon={checkmark} />
         </IonCard>
         

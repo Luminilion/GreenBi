bdd5074944883a8d4adb1bdd9bda061af0cf3d3b
luminilion <nico.darge@gmail.com>
2020-04-30 12:04:15 +0200

    Fixed description bug

--- client/src/data/ProjectData.tsx
+++ client/src/data/ProjectData.tsx
@@ -9,7 +9,7 @@ const ProjectData = {
             fund: 0.7,
             risque: "élevé",
             totalFund: 10000,
-            decription: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
+            description: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
             Ipsum set dolorem Lorem Ipsum set dolorem\
             Ipsum set dolorem Lorem Ipsum set dolorem\
             Ipsum set dolorem Lorem Ipsum set dolorem\
@@ -25,7 +25,7 @@ const ProjectData = {
             fund: 0.4,
             risque: "modéré",
             totalFund: 2000,
-            decription: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
+            description: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
             Ipsum set dolorem Lorem Ipsum set dolorem\
             Ipsum set dolorem Lorem Ipsum set dolorem\
             Ipsum set dolorem Lorem Ipsum set dolorem\
@@ -41,7 +41,7 @@ const ProjectData = {
             fund: 0.9,
             risque: "faible",
             totalFund: 58040,
-            decription: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
+            description: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
             Ipsum set dolorem Lorem Ipsum set dolorem\
             Ipsum set dolorem Lorem Ipsum set dolorem\
             Ipsum set dolorem Lorem Ipsum set dolorem\
@@ -57,7 +57,7 @@ const ProjectData = {
             fund: 0.6,
             risque: "faible",
             totalFund: 1040000,
-            decription: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
+            description: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
             Ipsum set dolorem Lorem Ipsum set dolorem\
             Ipsum set dolorem Lorem Ipsum set dolorem\
             Ipsum set dolorem Lorem Ipsum set dolorem\
@@ -74,7 +74,7 @@ const ProjectData = {
             risque: "faible",
             totalFund: 500000,
             investors: 75,
-            description: "Société de chimie verte, Carbios (code ISIN FR0011648716) a développé depuis sa création en 2015 deux bioprocédés industriels dans le domaine de la biodégradation et du biorecyclage des polymères. Ces innovations permettent d’optimiser les performances et le cycle de vie des plastiques et textiles en exploitant les propriétés d’enzymes. Le modèle de développement de Carbios s’appuie sur l’industrialisation et la commercialisation de ses produits et enzymes, de ses technologies et de ses bioprocédés. Invest Securities a récemment porté de 14,9 à 15,2 euros son objectif de cours sur la valeur, Carbios ayant annoncé avoir franchi une nouvelle étape dans le développement de son procédé de dépolymérisation enzymatique en le rendant applicable aux fibres de polyester PET (polyéthylène téréphtalate) des déchets textiles, s’ouvrant ainsi un marché d’envergure. Cette annonce nous conforte dans nos prévisions d’une forte génération de cash sur le PET à partir de 2021/2022. Néanmoins, pour parfaire le développement de son nouveau procédé, Carbios a besoin de fonds pour financer ses RD."
+            description: "Société de chimie verte, Carbios (code ISIN FR0011648716) a développé depuis sa création en 2015 deux bioprocédés industriels dans le domaine de la biodégradation et du biorecyclage des polymères. Ces innovations permettent d’optimiser les performances et le cycle de vie des plastiques et textiles en exploitant les propriétés d’enzymes. Le modèle de développement de Carbios s’appuie sur l’industrialisation et la commercialisation de ses produits et enzymes, de ses\ technologies et de ses bioprocédés. Invest Securities a récemment porté de 14,9 à 15,2 euros son objectif de cours sur la valeur, Carbios ayant annoncé avoir franchi une nouvelle étape dans le développement de son procédé de dépolymérisation enzymatique en le rendant applicable aux fibres de polyester PET (polyéthylène téréphtalate) des déchets textiles, s’ouvrant ainsi un marché d’envergure. Cette annonce nous conforte dans nos prévisions d’une forte génération de cash sur le PET \ à partir de 2021/2022. Néanmoins, pour parfaire le développement de son nouveau procédé, Carbios a besoin de fonds pour financer ses RD."
         },
     ],
     stocks : [
@@ -85,7 +85,7 @@ const ProjectData = {
           direction: "up",
           value: 12.9,
           risque: "modéré",
-          decription: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
+          description: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
           Ipsum set dolorem Lorem Ipsum set dolorem\
           Ipsum set dolorem Lorem Ipsum set dolorem\
           Ipsum set dolorem Lorem Ipsum set dolorem\
@@ -100,7 +100,7 @@ const ProjectData = {
           direction: "down",
           value: 37.5,
           risque: "élevé",
-          decription: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
+          description: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
           Ipsum set dolorem Lorem Ipsum set dolorem\
           Ipsum set dolorem Lorem Ipsum set dolorem\
           Ipsum set dolorem Lorem Ipsum set dolorem\
@@ -115,7 +115,7 @@ const ProjectData = {
           direction: "up",
           value: 93.2,
           risque: "élevé",
-          decription: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
+          description: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
           Ipsum set dolorem Lorem Ipsum set dolorem\
           Ipsum set dolorem Lorem Ipsum set dolorem\
           Ipsum set dolorem Lorem Ipsum set dolorem\
@@ -130,7 +130,7 @@ const ProjectData = {
           direction: "up",
           value: 34.1,
           risque: "faible",
-          decription: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
+          description: "Lorem Ipsum set dolorem Lorem Ipsum set dolorem Lorem \
           Ipsum set dolorem Lorem Ipsum set dolorem\
           Ipsum set dolorem Lorem Ipsum set dolorem\
           Ipsum set dolorem Lorem Ipsum set dolorem\
